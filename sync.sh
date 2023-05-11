01009e2b641141eb3af9496d5dd17e2314d1037d
Rikiub <jesusviloriaolivar@gmail.com>
2023-05-11 02:48:08 -0400

    files update

--- sync.sh
+++ sync.sh
@@ -1,9 +1,13 @@
 #/usr/bin/env sh
 
+# find and delete broken symlinks
 find $HOME -xtype l -delete
-stow -d $HOME/dotfiles -t $HOME default \
-default darkman fish mpv sway scripts
 
+# sync stow files
+stow -d $HOME/dotfiles -t $HOME \
+default fish scripts mpv darkman sway
+
+# push to my github account
 git add .
 git commit -m "files update"
 git push
