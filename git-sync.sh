a3485b8e12e95e2d0e462aca624ba71f641084d6
Rikiub <jesusviloriaolivar@gmail.com>
2023-05-11 12:54:01 -0400

    config update

---
+++ git-sync.sh
@@ -0,0 +1,9 @@
+#/usr/bin/env sh
+
+# cd to git repo
+cd $HOME/dotfiles
+
+# push to my github account
+git add .
+git commit -m "config update"
+git push
--- stow-sync.sh
+++ stow-sync.sh
@@ -6,8 +6,3 @@ find $HOME -xtype l -delete
 # sync stow files
 stow -d $HOME/dotfiles -t $HOME \
 default fish scripts mpv darkman sway
-
-# push to my github account
-git add .
-git commit -m "config update"
-git push
