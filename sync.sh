67334922ec858744bcabab95de78813e96952e09
Rikiub <jesusviloriaolivar@gmail.com>
2023-05-11 02:41:22 -0400

    files update

--- sync.sh
+++ sync.sh
@@ -1,7 +1,8 @@
 #/usr/bin/env sh
 
 find $HOME -xtype l -delete
-stow -d $HOME/dotfiles -t $HOME */
+stow -d $HOME/dotfiles -t $HOME default \
+default darkman fish mpv sway scripts
 
 git add .
 git commit -m "files update"
