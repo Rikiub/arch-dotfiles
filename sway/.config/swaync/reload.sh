9a6e743678759064917457f2bdc493071c3752bb
Rikiub <rikiub.dev@gmail.com>
2023-07-13 17:10:25 -0400

    update

---
+++ sway/.config/swaync/reload.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+IMG="/home/sus/Imágenes/__PERSONAL/Icons/rikiub.jpg"
+
+echo "> Reloading..."
+swaync-client -R
+swaync-client -rs
+
+echo "> Starting"
+
+ notify-send -i rngicon hi there
+# notify-send -i $(rngicon) "Test icon notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum gravida. Sed mollis neque felis, quis rutrum risus porttitor vel. Duis finibus lorem nec massa lacinia, quis vehicula est ultricies. Suspendisse consectetur orci a quam fringilla, id malesuada quam posuere. Vestibulum non ipsum consequat, venenatis turpis nec, viverra mi. Sed bibendum odio vel augue luctus congue."
+# notify-send "Test noicon notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum ..."
+# notify-send -u critical "Test critical notification" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut mauris quis ante bibendum ..."
+# notify-send -i $IMG hi there
+
+sleep 1
+swaync-client -t
