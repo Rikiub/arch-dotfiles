bae4980ac83439a700834661c3f762eb86e42d77
Rikiub <rikiub.dev@gmail.com>
2023-09-24 13:38:47 -0400

    update

--- pywal/.local/share/light-mode.d/wal-theme-switch.sh
+++ pywal/.local/share/light-mode.d/wal-theme-switch.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env sh
 
+wait
+
 # --- VARS ---
 
-TYPE="light"
+COLOR_SCHEME=$(gsettings get org.gnome.desktop.interface color-scheme)
+COLOR_SCHEME=$(echo $COLOR_SCHEME | sed "s/'//g")
+COLOR_SCHEME=$(echo $COLOR_SCHEME | sed -n 's/prefer-\(dark\|light\)/\1/p')
+
+echo "Swtiching to $COLOR_SCHEME"
+
 WAL=${XDG_CACHE_HOME:-$HOME/.cache}/wal
 CURRENT_WALLPAPER=$(command cat $WAL/wal)
 
 SCHEME=$(echo $CURRENT_WALLPAPER | tr '/\.' '_')
-SCHEME=$(echo "$WAL/schemes/$SCHEME""_$TYPE"*)
+SCHEME=$(echo "$WAL/schemes/$SCHEME""_$COLOR_SCHEME"*)
 
-WAL_COMMAND="wal -qnst -l"
+if [ $COLOR_SCHEME = "light" ]; then
+  WAL_COMMAND="wal -qnst -l"
+elif [ $COLOR_SCHEME = "dark" ]; then
+  WAL_COMMAND="wal -qnst"
+fi
 
 # --- MAIN ---
 
@@ -20,7 +31,7 @@ if [ -f "$SCHEME" ]; then
 
 # if not exist, check if wallpaper exist and create.
 elif [ -f $CURRENT_WALLPAPER ]; then
-  echo "Scheme not founded. Creating {$TYPE} theme..."
+  echo "Scheme not founded. Creating $COLOR_SCHEME theme..."
   $WAL_COMMAND -i $CURRENT_WALLPAPER
 
 # if nothing exist, got error. you need create a theme with "wal -i <path/to/image>"
