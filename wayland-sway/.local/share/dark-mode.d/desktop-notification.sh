854818e3ca449bfa0c854e47eb1427563aa36682
Rikiub <rikiub.dev@gmail.com>
2023-10-10 12:53:16 -0400

    update

--- wayland-sway/.local/share/dark-mode.d/desktop-notification.sh
+++ wayland-sway/.local/share/dark-mode.d/desktop-notification.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # trigger a small, passive popup dialog to inform the user about darkman's activity
 # reference https://wiki.archlinux.org/title/Desktop_notifications#Usage_in_programming
 
-notify-send --app-name="darkman" --urgency=low --replace-id=1 --icon=weather-clear-night \
-"Switching to dark mode"
+ID=$(notify-send -ept 1 ".")
+
+if [ $ID = 1 ]
+then
+  :
+else 
+  notify-send --app-name="darkman" --urgency=low --replace-id=1 --icon=weather-clear-night \
+  "Switching to dark mode"
+fi
--- wayland-sway/.local/share/light-mode.d/desktop-notification.sh
+++ wayland-sway/.local/share/light-mode.d/desktop-notification.sh
@@ -3,5 +3,12 @@
 # trigger a small, passive popup dialog to inform the user about darkman's activity
 # reference https://wiki.archlinux.org/title/Desktop_notifications#Usage_in_programming
 
-notify-send --app-name="darkman" --urgency=low --replace-id=1 --icon=weather-clear \
-"Switching to light mode"
+ID=$(notify-send -ept 1 ".")
+
+if [ $ID = 1 ]
+then
+  :
+else
+  notify-send --app-name="darkman" --urgency=low --replace-id=1 --icon=weather-clear \
+  "Switching to light mode"
+fi
