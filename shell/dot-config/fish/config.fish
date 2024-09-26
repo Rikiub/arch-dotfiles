a2cf6bb6e30a00396965ad783237192d191d7319
Rikiub <rikiub.dev@gmail.com>
2024-09-26 00:04:15 -0400

    Update

--- shell/dot-config/fish/config.fish
+++ shell/dot-config/fish/config.fish
@@ -4,3 +4,10 @@ end
 
 # Created by `pipx` on 2024-06-01 20:56:41
 set PATH $PATH /home/sus/.local/bin
+
+# pnpm
+set -gx PNPM_HOME "/home/sus/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
